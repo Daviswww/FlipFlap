0932088496a4aea156763528c98605efaaa8ff7e
daviswww <ho861206@gmail.com>
2020-10-08 10:46:55 +0800

    done😎

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,18 +3,16 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-    @data_s = tsv.to_s
-    # print @data_s
     @data = []
     tsv = tsv.split(/\n/)
     @title = tsv[0].split(/\t/)
     1.upto tsv.length - 1 do |i|
       tsv_line = tsv[i].split(/\t/)
-      @data << string_to_hash(tsv_line)
+      @data << tsv_to_hash(tsv_line)
     end
   end
 
-  def string_to_hash(tsv_line)
+  def tsv_to_hash(tsv_line)
     hash = {}
     0.upto @title.length - 1 do |j|
       hash[@title[j]] = tsv_line[j]
@@ -28,11 +26,7 @@ module TsvBuddy
     data_s = ''
     data_s += "#{tsv_title(@data)}\n"
     @data.each do |datas|
-      tsv_line = ''
-      datas.each do |key, value|
-        tsv_line += key == 'date' ? value.to_s : "\t#{value}"
-      end
-      data_s += "#{tsv_line}\n"
+      data_s += "#{hash_to_tsv(datas)}\n"
     end
     data_s
   end
@@ -43,5 +37,13 @@ module TsvBuddy
       title += key[0] == 'date' ? key[0].to_s : "\t#{key[0]}"
     end
     title
+  end
+
+  def hash_to_tsv(datas)
+    tsv_line = ''
+    datas.each do |key, value|
+      tsv_line += key == 'date' ? value.to_s : "\t#{value}"
+    end
+    tsv_line
   end
 end
